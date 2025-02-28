bfda034792ece0b02e55f498756866178f334a7e
yannu ⛧ <carlosemanueldesouza134@gmail.com>
2025-02-28 16:19:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const tabelaPersonagens = document.getElementById('tabelaPersonagens');
+    const cadastroMensagem = document.getElementById('cadastro-mensagem');
+    const formCadastro = document.getElementById('formCadastro');
 
     const dadosPadrao = [
         { nome: "Barbie", profissao: "Médica" },
         { nome: "Ken", profissao: "Clt" },
-        { nome: "Raquelle", profissao: "Cabeleleira" },
+        { nome: "Raquelle", profissao: "Cabeleireira" },
         { nome: "Skipper", profissao: "Sketista" },
         { nome: "Stacie", profissao: "Empreendedora" },
         { nome: "Chelsea", profissao: "Problemática" },
@@ -26,17 +28,27 @@ document.addEventListener('DOMContentLoaded', function() {
         tabelaPersonagens.appendChild(novaLinha);
     }
 
+    function mostrarMensagem(mensagem, tempo = 3000) {
+        cadastroMensagem.textContent = mensagem;
+        cadastroMensagem.style.display = 'block';
+        setTimeout(() => {
+            cadastroMensagem.style.display = 'none';
+        }, tempo);
+    }
+
     dadosPadrao.forEach(personagem => {
         adicionarLinha(personagem.nome, personagem.profissao);
     });
 
-    document.getElementById('formCadastro').addEventListener('submit', function(event) {
+    formCadastro.addEventListener('submit', function(event) {
         event.preventDefault();
 
         const nome = document.getElementById('nome').value;
         const profissao = document.getElementById('profissao').value;
 
         adicionarLinha(nome, profissao);
-        document.getElementById('formCadastro').reset();
+        formCadastro.reset();
+
+        mostrarMensagem('Parabéns! Você agora faz parte da Instituição Barbie');
     });
 });
